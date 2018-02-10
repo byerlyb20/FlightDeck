00ba48be3c52d67270b6b5ecd884295df0f7cf2c
Brigham Byerly <byerlybrigham@gmail.com>
2018-02-09 20:27:53 -0500

    Added color themes to app

--- app/src/main/java/com/brigham/badon/flightdeck/MainActivity.java
+++ app/src/main/java/com/brigham/badon/flightdeck/MainActivity.java
@@ -2,6 +2,7 @@ package com.brigham.badon.flightdeck;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Setup the toolbar
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
     }
 }
